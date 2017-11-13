b7a5b9f419198bf772e59cd971c709bc7ae8b668
andy mcghie <mcghiea@gmail.com>
2017-11-13 07:21:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return "Happy holidays, ${name}!"
 }
-function happyHolidayTo(holiday,name){
-  return "Happy ${holdiay}, ${name}!"
+function happyHolidayTo(holiday, name){
+  return "Happy ${holiday}, ${name}!"
 }
-function holidayCountdown(holiday, days){}
+function holidayCountdown(holiday, days){
+  return "It's ${days} days until ${holiday}!"
 }
